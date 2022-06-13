62a582b62629945fca7605b416ba00f50f97e5d4
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-13 12:29:25 +0300

    add update profile view

--- awards/views.py
+++ awards/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Project, Profile, Review
+from .models import *
 from .forms import ProfileForm, ProjectForm, RateForm
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProjectSerializer, ProfileSerializer
 
-# Create your views here.
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
