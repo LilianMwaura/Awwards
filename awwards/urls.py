89713096e015bfbaca998bd3f572df3c2ad23230
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-13 12:08:18 +0300

    update urls on logout

--- awwards/urls.py
+++ awwards/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('award.urls')),
+    path('', include ('awards.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
